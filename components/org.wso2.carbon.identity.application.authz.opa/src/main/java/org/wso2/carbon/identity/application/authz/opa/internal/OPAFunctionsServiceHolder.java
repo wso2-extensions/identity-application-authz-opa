0d60d0b0f724f09a1367f0d8da1db5b902702784
nimashanishadi <nimasha@wso2.com>
2020-04-09 11:27:30 +0530

    files were modified according to reviewed comments

---
+++ components/org.wso2.carbon.identity.application.authz.opa/src/main/java/org/wso2/carbon/identity/application/authz/opa/internal/OPAFunctionsServiceHolder.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.wso2.carbon.identity.application.authz.opa.internal;
+
+import org.wso2.carbon.identity.application.authentication.framework.JsFunctionRegistry;
+import org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataManagementService;
+
+/**
+ * ServiceHolder which handles  OPA related Functions.
+ */
+public class OPAFunctionsServiceHolder {
+
+    private static OPAFunctionsServiceHolder instance = new OPAFunctionsServiceHolder();
+    private JsFunctionRegistry jsFunctionRegistry;
+
+    public ClaimMetadataManagementService getClaimMetadataManagementService() {
+
+        return claimMetadataManagementService;
+    }
+
+    public void setClaimMetadataManagementService(ClaimMetadataManagementService claimMetadataManagementService) {
+
+        this.claimMetadataManagementService = claimMetadataManagementService;
+    }
+
+    private ClaimMetadataManagementService claimMetadataManagementService = null;
+
+    public static OPAFunctionsServiceHolder getInstance() {
+
+        return instance;
+    }
+
+    private OPAFunctionsServiceHolder() {
+
+    }
+
+    public JsFunctionRegistry getJsFunctionRegistry() {
+
+        return jsFunctionRegistry;
+    }
+
+    public void setJsFunctionRegistry(JsFunctionRegistry jsFunctionRegistry) {
+
+        this.jsFunctionRegistry = jsFunctionRegistry;
+    }
+
+}
